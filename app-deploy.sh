9731711542fbe0c1b41b8768b43d785a95ce81cf
kushaljk015 <gv@gvs-macbook-air.mynetworksettings.com>
2023-02-17 18:20:41 -0500

    rmoved hardcoded ecr repo details

--- app-deploy.sh
+++ app-deploy.sh
@@ -2,6 +2,6 @@
 
 ## docker image build and push
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $1
-docker build -t 508460187788.dkr.ecr.us-east-1.amazonaws.com/rearc-quest:latest
-docker push 508460187788.dkr.ecr.us-east-1.amazonaws.com/rearc-quest:latest
+docker build -t $1/rearc-quest:latest
+docker push $1/rearc-quest:latest
 
