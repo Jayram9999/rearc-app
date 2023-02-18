ed5da45d0f9b48b95eb174b0a2befcaaec6cebb6
govardhanmundlur <gv3br10ee032@gmail.com>
2023-02-18 07:54:33 -0500

    Merge branch 'main' of github.com:GovardhanMundlur/rearc-app

--- app-deploy.sh
+++ app-deploy.sh
@@ -5,3 +5,10 @@ aws ecr get-login-password --region us-east-1 | docker login --username AWS --pa
 docker build -t $1/rearc-quest:latest
 docker push $1/rearc-quest:latest
 
+## set kubeconfig and deploy k8s resources
+
+export KUBECONFIG=~/.kube/config
+kubectl apply -f k8s/external-dns/*
+kubectl apply -f k8s/alb-ingress/*
+kubectl apply -f k8s/appyamls/*
+
