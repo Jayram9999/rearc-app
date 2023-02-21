f4b6c30126e9c882afc08657f069f0ed1611d9f1
govardhanmundlur <gv3br10ee032@gmail.com>
2023-02-21 08:08:13 -0500

    added a yq command to modify the image tag

--- app-deploy.sh
+++ app-deploy.sh
@@ -13,12 +13,15 @@ new_tag_version=$((current_tag+1))
 echo $new_tag_version
 docker tag "$image_val" "$1/rearc-quest:v$new_tag_version"
 docker push "$1/rearc-quest:v$new_tag_version"
+new_image_tag=""$1/rearc-quest:v$new_tag_version""
 
 ## yq to update hte deployment file ##
+## not working yet, yq is not considering the $new_image_tag as one value ==> improvement
+#yq -i e ".spec.template.spec.containers.image |= '$new_image_tag'" k8s/appyamls/deployment.yaml
 
 ## set kubeconfig and deploy k8s resources
-export KUBECONFIG=~/.kube/config
-kubectl apply -f k8s/external-dns/*
-kubectl apply -f k8s/alb-ingress/*
-kubectl apply -f k8s/appyamls/*
+#export KUBECONFIG=~/.kube/config
+#kubectl apply -f k8s/external-dns/*
+#kubectl apply -f k8s/alb-ingress/*
+#kubectl apply -f k8s/appyamls/*
 
